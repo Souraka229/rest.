fcafef7ae5c0c12eb7f9bd5fcbab7f304953f1f8
Souraka229 <bagoudou046@gmail.com>
2025-11-08 09:33:45 +0100

    Create utils.ts

---
+++ lib/utils.ts
@@ -0,0 +1,22 @@
+// lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number): string {
+  return new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'XOF',
+  }).format(price)
+}
+
+export function generateSlug(name: string): string {
+  return name
+    .toLowerCase()
+    .replace(/[^a-z0-9 -]/g, '')
+    .replace(/\s+/g, '-')
+    .replace(/-+/g, '-')
+}
